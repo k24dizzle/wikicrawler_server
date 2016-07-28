05c60d1bf8ce384e68c92036739ed995439297b5
k24dizzle <zhaok24@uw.edu>
2016-07-28 11:26:28 -0700

    static

--- wikicrawler_server/settings.py
+++ wikicrawler_server/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, '../wikiapp/static'),)
+STATIC_ROOT = os.path.join('wikicrawler_server', 'static')
