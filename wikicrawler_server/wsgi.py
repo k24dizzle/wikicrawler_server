889f32d9e1ccb2b11e8a30df7812dd9c9576ac7a
k24dizzle <zhaok24@uw.edu>
2016-07-28 11:37:21 -0700

    using white noise

--- wikicrawler_server/wsgi.py
+++ wikicrawler_server/wsgi.py
@@ -1,17 +1,8 @@
-"""
-WSGI config for wikicrawler_server project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikicrawler_server.settings")
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
